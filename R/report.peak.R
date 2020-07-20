b509cd35f4ae414c4a072ccca85f0bcf703337fa
stasaki <stasaki@gmail.com>
2020-07-20 23:18:15 +0000

    remove dot

--- R/report.peak.R
+++ R/report.peak.R
@@ -12,3 +12,16 @@
   write.table(temp,file=paste(dir,"peak.bed",sep="/"), sep="\t",row.names =FALSE,quote = FALSE)
   
 }
+
+# comparing two experimental conditions
+report.peak<- function(TOTAL_PEAK_RESULT,PARAMETERS) {
+  
+  # get dir
+  dir=paste(PARAMETERS$OUTPUT_DIR,PARAMETERS$EXPERIMENT_NAME,sep='/')
+  
+  write.table(TOTAL_PEAK_RESULT,file=paste(dir,"peak.xls",sep="/"), sep="\t",row.names =FALSE,quote = FALSE)
+  temp=TOTAL_PEAK_RESULT[,1:12]
+  names(temp)[1]=paste("#",names(temp)[1])
+  write.table(temp,file=paste(dir,"peak.bed",sep="/"), sep="\t",row.names =FALSE,quote = FALSE)
+  
+}
