cdaf7b01a4c574804d8d638c1816f7289bcba2f3
stasaki <stasaki@gmail.com>
2020-07-21 16:35:50 +0000

    supply dir

--- R/report.peak.R
+++ R/report.peak.R
@@ -14,10 +14,10 @@
 }
 
 # comparing two experimental conditions
-report.peak<- function(TOTAL_PEAK_RESULT,PARAMETERS) {
+report.peak<- function(TOTAL_PEAK_RESULT,PARAMETERS, dir) {
   
   # get dir
-  dir=paste(PARAMETERS$OUTPUT_DIR,PARAMETERS$EXPERIMENT_NAME,sep='/')
+  #dir=paste(PARAMETERS$OUTPUT_DIR,sep='/')
   
   write.table(TOTAL_PEAK_RESULT,file=paste(dir,"peak.xls",sep="/"), sep="\t",row.names =FALSE,quote = FALSE)
   temp=TOTAL_PEAK_RESULT[,1:12]
